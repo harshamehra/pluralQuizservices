3325f416a7a70557d2a1cafc95e1d1790e49756c
harshamehra <34327069+harshamehra@users.noreply.github.com>
2018-03-27 15:03:49 -0400

    Create apicontroller.js

---
+++ controllers/apicontroller.js
@@ -0,0 +1,83 @@
+var mongoose = require('mongoose'),
+    Questions = require('../models/question'),
+    bodyParser = require('body-parser'),
+    JSONStream = require('JSONStream');
+    
+    
+    module.exports = function(app){
+    
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    
+    app.get('', function(req, res){
+        res.redirect('/questions');
+        
+    });
+    
+    
+    app.get('/questions', function(req, res){
+         Questions.find().stream()
+         .pipe(JSONStream.stringify()).pipe(res);
+         
+    });
+    
+    app.post('/new', function(req, res){
+        var count = 0;
+        var options = [];
+        console.log(JSON.stringify(req.body));
+        var optionList = req.body.options;
+        for(var i=0; i<optionList.length; i++){
+                options.push(optionList[i]);
+                count++;
+                if(count == optionList.length){
+                    var questionToSave = {
+                    question: req.body.question,
+                    options: options
+                        };
+                    Questions.create(questionToSave, function(err, body){
+                    if(err) 
+                    {
+                        console.log('There was aan error saving question');
+                        throw err;
+                    }
+                    res.send(body);
+                    res.end();
+                });
+            }
+        }
+
+    });
+
+    app.put("/edit/:id", function(req, res){
+        console.log("OK OK"+JSON.stringify(req.body));
+        console.log(JSON.stringify("Is this working?"+req.params.id));
+         Questions.findByIdAndUpdate(req.params.id, req.body, function(err, updatedQuestion){
+            if(err)
+            {
+                console.log(err);
+                
+            }
+            else
+            {
+            res.send(updatedQuestion);
+            }
+         });
+        
+    });
+    
+    app.delete("/delete/:id", function(req, res){
+        console.log("Is this working?"+JSON.stringify(req.params.id));
+         Questions.findByIdAndRemove(req.params.id, function(err, body){
+            if(err)
+            {
+                console.log(err);
+                
+            }
+            else
+            {
+            res.send('deleted');;
+            }
+         });
+        
+    });
+}
